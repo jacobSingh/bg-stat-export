f9305ae364fbc66491bbb16be313e2fb1e804882
Jacob Singh <jacobsingh@gmail.com>
2016-11-30 19:47:03 +0530

    Added shebang

--- _makeindex.sh
+++ _makeindex.sh
@@ -1,2 +1,3 @@
-ls | 
-perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>"; } print "</ul></body></html>"' > index.html
+#!/bin/bash
+ls -d */|
+perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_/index.html\">$_</a></li>"; } print "</ul></body></html>"' > index.html
