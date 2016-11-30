e605f5cc71b92c1f47b33a969035e38396192aee
Jacob Singh <jacobsingh@gmail.com>
2016-11-30 19:34:59 +0530

    Added NightHunter-JacobSingh-20161129-140116

--- _makeindex.sh
+++ _makeindex.sh
@@ -1,2 +1,2 @@
-ls | egrep '(jpg|png)' | \\ #filter the content you want in the index
+ls | 
 perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>"; } print "</ul></body></html>"' > index.html
